d4c78170f9d177706a3c56206b3015fc38884703
Nick Farley <nick@nfreader.net>
2014-01-31 12:36:48 -0500

    Muh tidy!

--- index.php
+++ index.php
@@ -1,117 +1,184 @@
 <?php require_once('header.php'); ?>
-     
-      <div class="section-header" id="connect">
-          <h1>Connecting</h1>
-          <p>Simply connect to <code>m.neffy.me</code> in your Minecraft client.</p>
-    </div>
 
-      <div class="section-header" id="rules">
-          <h1>The Rules</h1>
-          <p>The Law of the Land is intended to be as fair as possible. However, players that fail to adhere to these guidelines will be banned from playing on this server.</p>
-    </div>
-    <dl>
-        <dt><div class="num">1</div>Do not be a dick</dt>
-        <dd>Observe <a href="http://www.urbandictionary.com/define.php?term=Wheaton%27s%20Law" target="_blank">Wheaton&apos;s Law</a> at all times.</dd>
-
-        <dt><div class="num">2</div> Do not grief</dt>
-        <dd>If you didn't build it, don't touch it without asking permission.</dd>
-
-        <dt><div class="num">3</div> Do not steal</dt>
-        <dd>Same as before.</dd>
-
-        <dt><div class="num">4</div> Do not use client side mods</dt>
-        <dd>Mods, including x-ray texture packs that grant a tactical advantage are strictly forbidden. <em>This does not apply to OptiFine.</em></dd>
-
-        <dt><div class="num">5</div> Do not interfere with core services</dt>
-        <dd>This includes tearing up or tampering with footpaths, canals and minecart tracks.</dd>
-    </dl>
-
-    <div class="section-header" id="directive">
-          <h1>The Prime Directive</h1>
-  </div>
-
-  <p>The staff undertakes not to violate the <a href="http://en.wikipedia.org/wiki/Prime_Directive">Prime Directive</a>: the server and its community must be allowed to evolve naturally, other than where intervention is necessary.</p>
-
-    <div class="section-header" id="moderators">
-          <h1>Moderation Team</h1>
-      </div>
-          <p>Moderators are <strong>volunteers</strong> who have agreed to assist in keeping the server community healthy and happy. They have been granted the power to kick or ban players who break the rules, and roll back block changes in cases of griefing. Currently, there are two moderators: <strong>mrxak</strong> and <strong>mud212</strong>. Please do not hesitate to contact them if you need any assistance.</p>
-
-          <div class="section-header" id="admins">
-              <h1>Administration Team</h1>
-          </div>
-          <p>m.neffy.me is a labor of love. The administration team, like the moderators, have volunteered their time to make sure the server remains a pleasant experience. The current administrator team consists of:</p>
-
-          <dl>
-
-              <dt>nfreader</dt>
-              <dd>handles day-to-day administration tasks</dd>
-
-              <dt>prophile</dt>
-              <dd>oversees all technical aspects of the server software and related services</dd>
-
-          </dl>
-
-          <div class="section-header" id="help">
-              <h1>Getting Help</h1>
-          </div>
-          <p>The best way to get help with an issue may depend on what the issue is.</p>
-          <dl>
-    <dt>For active, ongoing griefing</dt>
-    <dd>Please contact a member of the moderation team in-game, or if none are available, contact us via <a href="chat.php">HipChat</a></dd>
-
-    <dt>For recently discovered griefing</dt>
-    <dd>Ask an in-game moderator to rollback the changes.</dd>
-
-    <dt>For technical issues</dt>
-    <dd>If the server is down or you are otherwise unable to connect, please contact us via <a href="chat.php">HipChat</a>, or email <a href="mailto:minecraft@nfreader.net">minecraft@nfreader.net</a>
-    </dd>
-
-    <dt>For all other issues</dt>
-    <dd>If the issue is in-game, contact an active moderator in game. Otherwise, contact us via <a href="chat.php">HipChat</a></dd>
-    </dl>
-
-    <div class="section-header" id="tech">
-        <h1>Technology</h1>
-    </div>
-
-    <p>The server runs on a <a href="https://www.digitalocean.com/?refcode=e8fee75698ff">DigitalOcean Cloud Server</a><small>(referral link)</small> with 2GB of RAM and dual cores. Thanks to the work of <a href="#admins">prophile</a>, the server is set up and provisioned with <a href="https://github.com/prophile/glory">Glory</a>. Management is handled through <a href="https://github.com/mcdevs/mark2">mark2</a>.</p>
-
-    <iframe src="http://ghbtns.com/github-btn.html?user=prophile&repo=glory&type=fork&count=true&size=large"
-  allowtransparency="true" frameborder="0" scrolling="0" width="95" height="30"></iframe>
-
-    <div class="section-header" id="contribute">
-        <h1>Contribute</h1>
-    </div>
-
-    <p>There are two ways to help make sure the server and the experience you enjoy run as smoothly as possible.</p>
-    <dl>
-    <dt>Financial contributions</dt>
-      <dd>If you are over the age of 18, you can help the server by contributing through PayPal or with Bitcoin.</dd>
-    <p>Our Bitcoin address is <code>12bziKajZ4YDHgkhzVwJqzVC2tP4Nu6Uyf</code></p>
-    <p><form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-<input type="hidden" name="cmd" value="_donations">
-<input type="hidden" name="business" value="HJHQZDZTVSDHA">
-<input type="hidden" name="lc" value="US">
-<input type="hidden" name="item_name" value="m.neffy.me">
-<input type="hidden" name="currency_code" value="USD">
-<input type="hidden" name="bn" value="PP-DonationsBF:btn_donate_SM.gif:NonHosted">
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-</form></p>
-
-  
-    <dt>Referral Contributions</dt>
-    <dd>The server host, Digital Ocean offers referral bonuses for new clients that sign up using a specific referral code. If you're in need of a good, cheap VPS, we highly recommend Digital Ocean. Our referral code is <a href="https://www.digitalocean.com/?refcode=e8fee75698ff" target="_blank">https://www.digitalocean.com/?refcode=e8fee75698ff</a>
-
-    <dt>Code contributions</dt>
-    <dd>The tools powering the server are open source and publicly available on GitHub. If you are so inclined, feel free to correct any errors you find and issue pull requests.
-    <p><iframe src="http://ghbtns.com/github-btn.html?user=prophile&repo=glory&type=fork&count=true&size=large"
-  allowtransparency="true" frameborder="0" scrolling="0" width="95" height="30"></iframe></p>
-
-
-
-
-    </dd>
+<div class="section-header" id="connect">
+	<h1>
+		Connecting
+	</h1>
+	<p>
+		Simply connect to <code>m.neffy.me</code> in your Minecraft client.
+	</p>
+</div>
+<div class="section-header" id="rules">
+	<h1>
+		The Rules
+	</h1>
+	<p>
+		The Law of the Land is intended to be as fair as possible. However, players that fail to adhere to these guidelines will be banned from playing on this server.
+	</p>
+</div>
+<dl>
+	<dt>
+		<div class="num">
+			1
+		</div>Do not be a dick
+	</dt>
+	<dd>
+		Observe <a href="http://www.urbandictionary.com/define.php?term=Wheaton%27s%20Law" target="_blank">Wheaton's Law</a> at all times.
+	</dd>
+	<dt>
+		<div class="num">
+			2
+		</div>Do not grief
+	</dt>
+	<dd>
+		If you didn't build it, don't touch it without asking permission.
+	</dd>
+	<dt>
+		<div class="num">
+			3
+		</div>Do not steal
+	</dt>
+	<dd>
+		Same as before.
+	</dd>
+	<dt>
+		<div class="num">
+			4
+		</div>Do not use client side mods
+	</dt>
+	<dd>
+		Mods, including x-ray texture packs that grant a tactical advantage are strictly forbidden. <em>This does not apply to OptiFine.</em>
+	</dd>
+	<dt>
+		<div class="num">
+			5
+		</div>Do not interfere with core services
+	</dt>
+	<dd>
+		This includes tearing up or tampering with footpaths, canals and minecart tracks.
+	</dd>
 </dl>
+<div class="section-header" id="directive">
+	<h1>
+		The Prime Directive
+	</h1>
+</div>
+<p>
+	The staff undertakes not to violate the <a href="http://en.wikipedia.org/wiki/Prime_Directive">Prime Directive</a>: the server and its community must be allowed to evolve naturally, other than where intervention is necessary.
+</p>
+<div class="section-header" id="moderators">
+	<h1>
+		Moderation Team
+	</h1>
+</div>
+<p>
+	Moderators are <strong>volunteers</strong> who have agreed to assist in keeping the server community healthy and happy. They have been granted the power to kick or ban players who break the rules, and roll back block changes in cases of griefing. Currently, there are two moderators: <strong>mrxak</strong> and <strong>mud212</strong>. Please do not hesitate to contact them if you need any assistance.
+</p>
+<div class="section-header" id="admins">
+	<h1>
+		Administration Team
+	</h1>
+</div>
+<p>
+	m.neffy.me is a labor of love. The administration team, like the moderators, have volunteered their time to make sure the server remains a pleasant experience. The current administrator team consists of:
+</p>
+<dl>
+	<dt>
+		nfreader
+	</dt>
+	<dd>
+		handles day-to-day administration tasks
+	</dd>
+	<dt>
+		prophile
+	</dt>
+	<dd>
+		oversees all technical aspects of the server software and related services
+	</dd>
+</dl>
+<div class="section-header" id="help">
+	<h1>
+		Getting Help
+	</h1>
+</div>
+<p>
+	The best way to get help with an issue may depend on what the issue is.
+</p>
+<dl>
+	<dt>
+		For active, ongoing griefing
+	</dt>
+	<dd>
+		Please contact a member of the moderation team in-game, or if none are available, contact us via <a href="chat.php">HipChat</a>
+	</dd>
+	<dt>
+		For recently discovered griefing
+	</dt>
+	<dd>
+		Ask an in-game moderator to rollback the changes.
+	</dd>
+	<dt>
+		For technical issues
+	</dt>
+	<dd>
+		If the server is down or you are otherwise unable to connect, please contact us via <a href="chat.php">HipChat</a>, or email <a href="mailto:minecraft@nfreader.net">minecraft@nfreader.net</a>
+	</dd>
+	<dt>
+		For all other issues
+	</dt>
+	<dd>
+		If the issue is in-game, contact an active moderator in game. Otherwise, contact us via <a href="chat.php">HipChat</a>
+	</dd>
+</dl>
+<div class="section-header" id="tech">
+	<h1>
+		Technology
+	</h1>
+</div>
+<p>
+	The server runs on a <a href="https://www.digitalocean.com/?refcode=e8fee75698ff">DigitalOcean Cloud Server</a><small>(referral link)</small> with 2GB of RAM and dual cores. Thanks to the work of <a href="#admins">prophile</a>, the server is set up and provisioned with <a href="https://github.com/prophile/glory">Glory</a>. Management is handled through <a href="https://github.com/mcdevs/mark2">mark2</a>.
+</p>
+<p>
+	<iframe src="http://ghbtns.com/github-btn.html?user=prophile&amp;repo=glory&amp;type=fork&amp;count=true&amp;size=large" allowtransparency="true" frameborder="0" scrolling="0" width="95" height="30"></iframe>
+</p>
+<div class="section-header" id="contribute">
+	<h1>
+		Contribute
+	</h1>
+</div>
+<p>
+	There are two ways to help make sure the server and the experience you enjoy run as smoothly as possible.
+</p>
+<dl>
+	<dt>
+		Financial contributions
+	</dt>
+	<dd>
+		If you are over the age of 18, you can help the server by contributing through PayPal or with Bitcoin.
+	</dd>
+	<dd>
+		<p>
+			Our Bitcoin address is <code>12bziKajZ4YDHgkhzVwJqzVC2tP4Nu6Uyf</code>
+		</p>
+		<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+			<input type="hidden" name="cmd" value="_donations" /> <input type="hidden" name="business" value="HJHQZDZTVSDHA" /> <input type="hidden" name="lc" value="US" /> <input type="hidden" name="item_name" value="m.neffy.me" /> <input type="hidden" name="currency_code" value="USD" /> <input type="hidden" name="bn" value="PP-DonationsBF:btn_donate_SM.gif:NonHosted" /> <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" /> <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+		</form>
+	</dd>
+	<dt>
+		Additionally...
+	</dt>
+	<dd>
+		The server host, Digital Ocean offers referral bonuses for new clients that sign up using a specific referral code. If you're in need of a good, cheap VPS, we highly recommend Digital Ocean. Our referral code is <a href="https://www.digitalocean.com/?refcode=e8fee75698ff" target="_blank">https://www.digitalocean.com/?refcode=e8fee75698ff</a>
+	</dd>
+	<dt>
+		Code contributions
+	</dt>
+	<dd>
+		The tools powering the server are open source and publicly available on GitHub. If you are so inclined, feel free to correct any errors you find and issue pull requests.
+		<p>
+			<iframe src="http://ghbtns.com/github-btn.html?user=prophile&amp;repo=glory&amp;type=fork&amp;count=true&amp;size=large" allowtransparency="true" frameborder="0" scrolling="0" width="95" height="30"></iframe>
+		</p>
+	</dd>
+</dl>
+
 <?php require_once('footer.php'); ?>
